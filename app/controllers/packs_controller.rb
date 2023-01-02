e86e7173343a5ac1417668e2384f8c6771b1d8ed
sabi <zentixzaneic@gmail.com>
2023-01-02 14:19:23 -0600

    add ability to edit packs

--- app/controllers/packs_controller.rb
+++ app/controllers/packs_controller.rb
@@ -21,6 +21,20 @@ class PacksController < ApplicationController
     end
   end
 
+  def edit
+    @pack = Pack.find(params[:id])
+  end
+
+  def update
+    @pack = Pack.find(params[:id])
+
+    if @pack.update(pack_params)
+      redirect_to @pack
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
     def pack_params
       params.require(:pack).permit(:title, :download_link, :songs, :stepartists, :minlvl_target, :maxlvl_target, :minlvl_total, :maxlvl_total, :bpm_min, :bpm_max, :difficulties_min, :difficulties_max, :play_type, :play_format, :ranked)
