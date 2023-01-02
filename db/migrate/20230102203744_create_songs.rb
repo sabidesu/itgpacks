b81d5517d0d32e4d70464729ae2f5e9b459748b8
sabi <zentixzaneic@gmail.com>
2023-01-02 15:00:53 -0600

    distinguish between single and double difficulties

--- db/migrate/20230102203744_create_songs.rb
+++ db/migrate/20230102203744_create_songs.rb
@@ -6,12 +6,18 @@ class CreateSongs < ActiveRecord::Migration[7.0]
       t.integer :bpm
       t.integer :length
       t.string :stepartist
-      t.integer :novice
-      t.integer :easy
-      t.integer :medium
-      t.integer :hard
-      t.integer :expert
-      t.integer :edit
+      t.integer :single_novice
+      t.integer :single_easy
+      t.integer :single_medium
+      t.integer :single_hard
+      t.integer :single_expert
+      t.integer :single_edit
+      t.integer :double_novice
+      t.integer :double_easy
+      t.integer :double_medium
+      t.integer :double_hard
+      t.integer :double_expert
+      t.integer :double_edit
       t.references :pack, null: false, foreign_key: true
 
       t.timestamps
