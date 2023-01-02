e66cd94930d94bccb0e7a5d3cd16e636dddb4464
sabi <zentixzaneic@gmail.com>
2023-01-02 03:11:31 -0600

    modify fields on packs

--- db/migrate/20230102085536_create_packs.rb
+++ db/migrate/20230102085536_create_packs.rb
@@ -2,6 +2,7 @@ class CreatePacks < ActiveRecord::Migration[7.0]
   def change
     create_table :packs do |t|
       t.string :title
+      t.string :download
       t.integer :songs
       t.string :stepartists
       t.integer :minlvl_target
@@ -12,9 +13,9 @@ class CreatePacks < ActiveRecord::Migration[7.0]
       t.integer :bpm_max
       t.integer :difficulties_min
       t.integer :difficulties_max
-      t.integer :type
+      t.integer :play_type
+      t.integer :play_format
       t.boolean :ranked
-      t.string :download
 
       t.timestamps
     end
