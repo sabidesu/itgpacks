0ef10874800142c40f84e0b1f77538ee89d082e8
sabi <zentixzaneic@gmail.com>
2023-01-02 02:44:38 -0600

    add packs view

---
+++ app/controllers/packs_controller.rb
@@ -0,0 +1,4 @@
+class PacksController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "packs#index"
+  get "/packs", to: "packs#index"
 end
