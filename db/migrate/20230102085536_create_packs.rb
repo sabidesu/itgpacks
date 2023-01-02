ed12ceb4da6e5ca3453799f8714863968c66431b
sabi <zentixzaneic@gmail.com>
2023-01-02 02:57:53 -0600

    create pack model

---
+++ db/migrate/20230102085536_create_packs.rb
@@ -0,0 +1,21 @@
+class CreatePacks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :packs do |t|
+      t.string :title
+      t.integer :songs
+      t.string :stepartists
+      t.integer :minlvl_target
+      t.integer :maxlvl_target
+      t.integer :minlvl_total
+      t.integer :maxlvl_total
+      t.integer :bpm_min
+      t.integer :bpm_max
+      t.integer :difficulties_min
+      t.integer :difficulties_max
+      t.integer :type
+      t.boolean :ranked
+
+      t.timestamps
+    end
+  end
+end
