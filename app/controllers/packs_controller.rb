0430d317a390e631567ce5b9efd255a9d4eb6df8
sabi <zentixzaneic@gmail.com>
2023-01-02 13:52:19 -0600

    add way to create pack

--- app/controllers/packs_controller.rb
+++ app/controllers/packs_controller.rb
@@ -6,4 +6,23 @@ class PacksController < ApplicationController
   def show
     @pack = Pack.find(params[:id])
   end
+
+  def new
+    @pack = Pack.new
+  end
+
+  def create
+    @pack = Pack.new(pack_params)
+
+    if @pack.save
+      redirect_to @pack
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def pack_params
+      params.require(:pack).permit(:title, :download_link, :songs, :stepartists, :minlvl_target, :maxlvl_target, :minlvl_total, :maxlvl_total, :bpm_min, :bpm_max, :difficulties_min, :difficulties_max, :play_type, :play_format, :ranked)
+    end
 end
--- app/models/pack.rb
+++ app/models/pack.rb
@@ -1,2 +1,16 @@
 class Pack < ApplicationRecord
+  validates :title, presence: true
+  validates :download_link, presence: true
+  validates :songs, numericality: { only_integer: true }
+  validates :stepartists, presence: true
+  validates :minlvl_target, numericality: { only_integer: true }
+  validates :maxlvl_target, numericality: { only_integer: true }
+  validates :minlvl_total, numericality: { only_integer: true }
+  validates :maxlvl_total, numericality: { only_integer: true }
+  validates :bpm_min, numericality: { only_integer: true }
+  validates :bpm_max, numericality: { only_integer: true }
+  validates :difficulties_min, numericality: { only_integer: true }
+  validates :difficulties_max, numericality: { only_integer: true }
+  validates :play_type, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 6 }
+  validates :play_format, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 2 }
 end
